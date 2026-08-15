2f74403ac1c5b9b959fb45f1d7a939ac9cbd4738
ProMonnan <nfnmema@gmail.com>
2026-08-15 23:55:05 +0530

    Version Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "app.sahal.getsauce"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         vectorDrawables.useSupportLibrary = true
     }
